9a8cd97bd453eeb45283167d8b4f5507c2d4ffc7
Glenn Dollard <gdollard@cartrawler.com>
2023-03-21 14:33:41 +0000

    tiring now

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,7 +2,6 @@ def sayHello(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
 
-def saySomehingElse(message) {
-    def msg = message ? message : "Nada to say!"
-    sh "echo ${msg}"
+def saySomehingElse(message="Nothing to say!") {
+    sh "echo ${message}"
 }
