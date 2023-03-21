6fd879d6bb0607b6903f9acdb454f05370f6afc5
Glenn Dollard <gdollard@cartrawler.com>
2023-03-21 14:16:07 +0000

    add another function

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,8 @@
-def call(Map config = [:]) {
+def sayHello(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+}
+
+def saySomehingElse(message) {
+    def msg = message ? message : "Nada to say!"
+    sh "echo ${msg}"
 }
